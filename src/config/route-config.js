a6ae0a6c82cc35f5e80f7f2d64dd3991bc659748
Cristian Perez <cristiangp777@gmail.com>
2019-04-10 17:35:43 -0700

    added calendar

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -3,8 +3,9 @@ module.exports = {
     const staticRoutes = require("../routes/static");
     const blogRoutes = require("../routes/blog");
     const contactRoutes = require("../routes/contact");
+        const calendarRoutes = require("../routes/calendar");
 
-
+    app.use(calendarRoutes);
     app.use(contactRoutes);
     app.use(blogRoutes);
     app.use(staticRoutes);
