aee0e22df8cb2482fd991c2036691fb8db96697e
Cristian Perez <cristiangp777@gmail.com>
2019-04-11 14:32:59 -0700

    get messages spec working

--- spec/integration/messages_spec.js
+++ spec/integration/messages_spec.js
@@ -1,17 +1,46 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/messages";
+const sequelize = require("../../src/db/models/index").sequelize;
+ const Message = require("../../src/db/models").Message;
 
 describe("routes : messages", () => {
+  beforeEach((done) => {
+     this.message;
+     sequelize.sync({force: true}).then((res) => {
+
+      Message.create({
+        name: "John",
+        content: "There is a lot of them",
+        email: "dadada@dada.com",
+        phone: 1234567890
+      })
+       .then((message) => {
+         this.message = message;
+         done();
+       })
+       .catch((err) => {
+         console.log(err);
+         done();
+       });
+
+     });
+
+   });
 
   describe("GET /messages", () => {
 
-    it("should return a status code 200", (done) => {
-      request.get(base, (err, res, body) => {
-        expect(res.statusCode).toBe(200);
-        done();
-      });
-    });
+    it("should return a status code 200 and all messages", (done) => {
+
+//#3
+       request.get(base, (err, res, body) => {
+         expect(res.statusCode).toBe(200);
+         expect(err).toBeNull();
+         expect(body).toContain("John");
+      
+         done();
+       });
+     });
+   });
 
   });
-});
--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,7 +1,17 @@
+ const messageQueries = require("../db/queries.messages.js");
+
 module.exports = {
   index(req, res, next){
-    res.render("messages/index", {title: "Messages"});
-  },
+    messageQueries.getAllMessages((err, messages) => {
+
+//#3
+        if(err){
+          res.redirect(500, "static/index");
+        } else {
+          res.render("messages/index", {messages});
+        }
+      })
+    },
 
   new(req, res, next){
     res.render("messages/new", {title: "Contact Us"});
