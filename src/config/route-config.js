3c388ff1b701570a31eada67cdbc2f5c65c843c5
Cristian Perez <cristiangp777@gmail.com>
2019-04-13 16:56:20 -0700

    create user working

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -4,10 +4,12 @@ module.exports = {
     const blogRoutes = require("../routes/blog");
     const messageRoutes = require("../routes/messages");
     const calendarRoutes = require("../routes/calendar");
+     const userRoutes = require("../routes/users");
 
     app.use(calendarRoutes);
     app.use(messageRoutes);
     app.use(blogRoutes);
     app.use(staticRoutes);
+     app.use(userRoutes);
   }
 }
